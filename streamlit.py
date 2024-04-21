1bfef99bc9a74ae1afa8777bbabb5777474440d7
AnirudhJS1 <167625295+AnirudhJS1@users.noreply.github.com>
2024-04-21 11:09:15 -0400

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+
+# In[2]:
+
+
+a = st.number_input("Enter the first number")
+b = st.number_input("Enter the second number")
+c = st.number_input("Enter the third number")
+
+
+# In[4]:
+
+
+if a<b and b<c:
+    st.write("Third number is the greatest")
+elif b>a and b>c:
+    st.write("Second number is the greatest")
+else:
+    st.write("First number is the greatest")
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
