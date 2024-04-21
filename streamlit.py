0a5cdd4946eea7b0b3eeb636fb3d7c8fe66766b5
AnirudhJS1 <167625295+AnirudhJS1@users.noreply.github.com>
2024-04-21 11:32:36 -0400

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,11 +19,11 @@ c = st.number_input("Enter the third number")
 
 
 if a>=b and a>=c:
-    st.write("Third number is the greatest")
+    st.write("First number is the greatest")
 elif b>=a and b>=c:
     st.write("Second number is the greatest")
-elif c>=a and c>=b:
-    st.write("First number is the greatest")
+else:
+    st.write("Third number is the greatest")
 
 
 # In[3]:
